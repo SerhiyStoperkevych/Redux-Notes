8da9b9d012cff15a7771b32f07ee7a7fee0cece7
SerhiyStoperkevych <116464169+SerhiyStoperkevych@users.noreply.github.com>
2024-07-03 20:57:12 -0400

    Create GlobalState.js

---
+++ src/context/GlobalState.js
@@ -0,0 +1,28 @@
+import React, { createContext, useReducer } from 'react';
+
+const initialState = {
+  count: 0
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 };
+    case 'DECREMENT':
+      return { ...state, count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+export const GlobalContext = createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ state, dispatch }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
